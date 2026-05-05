84bd41a0be8fdb44915d40c529f9cf891e130b76
hitesh-patgar <opticallol81@gmail.com>
2026-05-06 00:57:20 +0530

    Add port support for Railway database connection

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import os
 def get_db_connection():
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", "#mysql24"),
         database=os.getenv("DB_NAME", "lost_found_db")
